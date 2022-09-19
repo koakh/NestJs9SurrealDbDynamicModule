32198acc1bd399b1bad024d4bcbd5f414ec13ca9
Mário Monteiro <marioammonteiro@gmail.com>
2022-09-19 23:58:22 +0100

    base service working with restaurants and recipes at same time

--- packages/app-gql/src/common/services/base.service.ts
+++ packages/app-gql/src/common/services/base.service.ts
@@ -1,8 +1,7 @@
 import { SurrealDbService } from '@koakh/nestjs-surrealdb';
 import { Injectable, Type } from '@nestjs/common';
-import { CreateRestaurantInput } from '../../restaurants/dto';
+import { CreateRestaurantInput, UpdateRestaurantInput } from '../../restaurants/dto';
 import { BaseFindAllArgs } from '../dto/base-find-all.args';
-import { BaseUpdateEntityInput } from '../dto/base-update-entity.input';
 import { BaseEntity } from '../entities';
 
 // T EntityType ex Recipe
@@ -10,17 +9,13 @@ import { BaseEntity } from '../entities';
 // V EntityArgs ex RecipesArgs
 // Z UpdateEntityInput ex UpdateRecipeInput
 @Injectable()
-export abstract class BaseService<T extends Type<BaseEntity>, K extends CreateRestaurantInput, V extends BaseFindAllArgs, Z extends BaseUpdateEntityInput> {
-  // constructor(protected readonly surrealDb: SurrealDbService, @Optional() classRef: T) {}
-  // TODO: it will uses child service and works, there is no need to pass in constructor
+export abstract class BaseService<T extends Type<BaseEntity>, K extends CreateRestaurantInput, V extends BaseFindAllArgs, Z extends UpdateRestaurantInput> {
   protected readonly surrealDb: SurrealDbService;
-  protected symbol: T;
-
-  // constructor(surrealDb: SurrealDbService, classRef: T) {}
+  protected entityName: T;
 
   async create(data: K): Promise<T> {
     // await this.surrealDb.thingExists(data.restaurant);
-    const id = data?.id ? data.id : this.symbol.name.toLowerCase();
+    const id = data?.id ? data.id : this.entityName.name.toLowerCase();
     return (await this.surrealDb.create(id, {
       ...data,
       createdAt: new Date(),
@@ -28,36 +23,33 @@ export abstract class BaseService<T extends Type<BaseEntity>, K extends CreateRe
     })) as T;
   }
 
-  // // TODO: almost equal just use generics here or a base class
-  // async findMany({ filter, skip, take }: V): Promise<T[]> {
-  //   // TODO: add surrealDb helper method with this sql in constants
-  //   const where = filter ? ` WHERE ${filter} ` : '';
-  //   const limit = take != undefined ? ` LIMIT ${take}` : '';
-  //   const start = skip != undefined ? ` START ${skip}` : '';
-  //   const query = `SELECT * FROM type::table($table)${where}${limit}${start};`;
-  //   // TODO: use vars with LIMIT and START
-  //   // const query = 'SELECT * FROM type::table($table) LIMIT $limit START $start';
-  //   const vars = { table: Recipe.name.toLowerCase(), start: skip, limit: take };
-  //   const data = await this.surrealDb.query(query, vars);
-  //   return data[0].result;
-  // }
+  async findMany({ filter, skip, take }: V): Promise<T[]> {
+    const where = filter ? ` WHERE ${filter} ` : '';
+    const limit = take != undefined ? ` LIMIT ${take}` : '';
+    const start = skip != undefined ? ` START ${skip}` : '';
+    const query = `SELECT * FROM type::table($table)${where}${limit}${start};`;
+    // TODO: use vars with LIMIT and START
+    // const query = 'SELECT * FROM type::table($table) LIMIT $limit START $start';
+    const vars = { table: this.entityName.name.toLowerCase(), start: skip, limit: take };
+    const data = await this.surrealDb.query(query, vars);
+    return data[0].result;
+  }
 
-  // // TODO: almost equal just use generics here or a base class
-  // async findOne(id: string): Promise<Recipe[]> {
-  //   const data = await this.surrealDb.select(id);
-  //   // should not get here if we pass above validation
-  //   if (data && Array.isArray(data) && data.length > 1) {
-  //     throw new Error('found more than one record');
-  //   }
-  //   return data && Array.isArray(data) && data.length === 1 ? (data[0] as unknown as Recipe[]) : [];
-  // }
+  async findOne(id: string): Promise<T[]> {
+    const data = await this.surrealDb.select(id);
+    // should not get here if we pass above validation
+    if (data && Array.isArray(data) && data.length > 1) {
+      throw new Error('found more than one record');
+    }
+    return data && Array.isArray(data) && data.length === 1 ? (data[0] as unknown as T[]) : [];
+  }
 
-  // async update(id: string, data: UpdateRecipeInput): Promise<Recipe> {
-  //   return (await this.surrealDb.change(id, data)) as any as Recipe;
-  // }
+  async update(id: string, data: Z): Promise<T> {
+    return (await this.surrealDb.change(id, data)) as any as T;
+  }
 
-  // async remove(id: string): Promise<boolean> {
-  //   await this.surrealDb.delete(id);
-  //   return true;
-  // }
+  async remove(id: string): Promise<boolean> {
+    await this.surrealDb.delete(id);
+    return true;
+  }
 }
--- packages/app-gql/src/restaurants/restaurants.service.ts
+++ packages/app-gql/src/restaurants/restaurants.service.ts
@@ -2,15 +2,14 @@ import { SurrealDbService } from '@koakh/nestjs-surrealdb';
 import { Injectable, Type } from '@nestjs/common';
 import { BaseFindAllArgs } from '../common/dto/base-find-all.args';
 import { BaseService } from '../common/services';
-import { UpdateRecipeInput } from '../recipes/dto';
 import { CreateRestaurantInput, UpdateRestaurantInput } from './dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Injectable()
-export class RestaurantsService extends BaseService<Type<Restaurant>, CreateRestaurantInput, BaseFindAllArgs, UpdateRecipeInput> {
+export class RestaurantsService extends BaseService<Type<Restaurant>, CreateRestaurantInput, BaseFindAllArgs, UpdateRestaurantInput> {
   constructor(protected readonly surrealDb: SurrealDbService) {
     super();
-    this.symbol = Restaurant;
+    this.entityName = Restaurant;
   }
 
   // TODO: already in BaseService and Working
@@ -22,41 +21,41 @@ export class RestaurantsService extends BaseService<Type<Restaurant>, CreateRest
   //   })) as Restaurant;
   // }
 
-  // TODO: almost equal just use generics here or a base class
-  async findMany({ filter, skip, take }: BaseFindAllArgs): Promise<Restaurant[]> {
-    // TODO: add surrealDb helper method with this sql in constants
-    const where = filter ? ` WHERE ${filter}` : '';
-    const limit = take != undefined ? ` LIMIT ${take}` : '';
-    const start = skip != undefined ? ` START ${skip}` : '';
-    const query = `SELECT * FROM type::table($table)${where}${limit}${start};`;
-    // TODO: use vars with LIMIT and START
-    // const query = 'SELECT * FROM type::table($table) LIMIT $limit START $start';
-    const vars = {
-      table: Restaurant.name.toLowerCase(),
-      start: skip,
-      limit: take,
-    };
-    // must work with both, data[0].result[0] work with resolveField
-    const data = await this.surrealDb.query(query, vars);
-    return data[0].result;
-  }
+  // // TODO: almost equal just use generics here or a base class
+  // async findMany({ filter, skip, take }: BaseFindAllArgs): Promise<Restaurant[]> {
+  //   // TODO: add surrealDb helper method with this sql in constants
+  //   const where = filter ? ` WHERE ${filter}` : '';
+  //   const limit = take != undefined ? ` LIMIT ${take}` : '';
+  //   const start = skip != undefined ? ` START ${skip}` : '';
+  //   const query = `SELECT * FROM type::table($table)${where}${limit}${start};`;
+  //   // TODO: use vars with LIMIT and START
+  //   // const query = 'SELECT * FROM type::table($table) LIMIT $limit START $start';
+  //   const vars = {
+  //     table: Restaurant.name.toLowerCase(),
+  //     start: skip,
+  //     limit: take,
+  //   };
+  //   // must work with both, data[0].result[0] work with resolveField
+  //   const data = await this.surrealDb.query(query, vars);
+  //   return data[0].result;
+  // }
 
-  // TODO: almost equal just use generics here or a base class
-  async findOne(id: string): Promise<Restaurant[]> {
-    const data = await this.surrealDb.select(id);
-    // should not get here if we pass above validation
-    if (data && Array.isArray(data) && data.length > 1) {
-      throw new Error('found more than one record');
-    }
-    return data && Array.isArray(data) && data.length === 1 ? (data[0] as unknown as Restaurant[]) : [];
-  }
+  // // TODO: almost equal just use generics here or a base class
+  // async findOne(id: string): Promise<Restaurant[]> {
+  //   const data = await this.surrealDb.select(id);
+  //   // should not get here if we pass above validation
+  //   if (data && Array.isArray(data) && data.length > 1) {
+  //     throw new Error('found more than one record');
+  //   }
+  //   return data && Array.isArray(data) && data.length === 1 ? (data[0] as unknown as Restaurant[]) : [];
+  // }
 
-  async update(id: string, data: UpdateRestaurantInput): Promise<Restaurant> {
-    return (await this.surrealDb.change(id, { ...data, updatedAt: new Date() })) as any as Restaurant;
-  }
+  // async update(id: string, data: UpdateRestaurantInput): Promise<Restaurant> {
+  //   return (await this.surrealDb.change(id, { ...data, updatedAt: new Date() })) as any as Restaurant;
+  // }
 
-  async remove(id: string): Promise<boolean> {
-    await this.surrealDb.delete(id);
-    return true;
-  }
+  // async remove(id: string): Promise<boolean> {
+  //   await this.surrealDb.delete(id);
+  //   return true;
+  // }
 }
