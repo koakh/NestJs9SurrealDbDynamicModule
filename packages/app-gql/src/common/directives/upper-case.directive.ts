d02073c41105707b665a0683a303fe33b53fb1b8
Mário Monteiro <marioammonteiro@gmail.com>
2022-09-29 22:43:03 +0100

    refactor app to app-rst

--- packages/app-gql/src/common/directives/upper-case.directive.ts
+++ packages/app-gql/src/common/directives/upper-case.directive.ts
@@ -12,7 +12,7 @@ export function upperDirectiveTransformer(schema: GraphQLSchema, directiveName: 
         // Replace the original resolver with a function that *first* calls
         // the original resolver, then converts its result to upper case
         fieldConfig.resolve = async function (source, args, context, info) {
-          const result = await resolve(source, args, context, info);
+          const result = resolve(source, args, context, info);
           if (typeof result === 'string') {
             return result.toUpperCase();
           }
