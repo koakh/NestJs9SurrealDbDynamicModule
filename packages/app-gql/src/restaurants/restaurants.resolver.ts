376da445f6da4d6679fbbd5a9ac8eb46d9053f31
Mário Monteiro <marioammonteiro@gmail.com>
2022-09-29 22:10:11 +0100

    finished restaurant dataLoader with array of recipes

--- packages/app-gql/src/restaurants/restaurants.resolver.ts
+++ packages/app-gql/src/restaurants/restaurants.resolver.ts
@@ -14,19 +14,22 @@ export class RestaurantsResolver extends BaseResolver(Restaurant, CreateRestaura
     super(restaurantsService);
   }
 
-  @ResolveField()
-  async recipes(@Parent() restaurant: Restaurant, @Loader(RecipeDataLoader) recipeDataLoader) {
-    const { id } = restaurant;
-    // batch query using dataloader, optional cast to string
-    return recipeDataLoader.load(id as unknown as string);
-  }
+  // both with and without resolveFields use same queries, keep with dataLoader for a future example of how to do it
+
+  // resolveField with dataLoader
+  // @ResolveField()
+  // async recipes(@Parent() restaurant: Restaurant, @Loader(RecipeDataLoader) recipeDataLoader) {
+  //   const { id } = restaurant;
+  //   // batch query using dataloader, optional cast to string
+  //   return recipeDataLoader.load(id as unknown as string);
+  // }
 
   // resolveField without dataLoader
-  // @ResolveField()
-  // async recipes(@Parent() restaurant: Restaurant) {
-  //   const { id } = restaurant;
-  //   return this.recipesService.findMany({
-  //     filter: `restaurant=${id}`,
-  //   });
-  // }
+  @ResolveField()
+  async recipes(@Parent() restaurant: Restaurant) {
+    const { id } = restaurant;
+    return this.recipesService.findMany({
+      filter: `restaurant=${id}`,
+    });
+  }
 }
