8217da3ca14eab427d13948be3936b1377432dc3
Mário Monteiro <marioammonteiro@gmail.com>
2022-10-03 22:53:40 +0100

    fix all yarn warns in peer dependencies

--- backup.sh
+++ backup.sh
@@ -5,16 +5,17 @@ DIR=.bak
 FILE="$DIR/$DT.tgz"
 FILE_EXCLUDE=exclude.tag
 mkdir $DIR -p
-touch .bak/$FILE_EXCLUDE
-touch .removed//$FILE_EXCLUDE
-touch node_modules/$FILE_EXCLUDE
-touch packages/app-lib/node_modules/$FILE_EXCLUDE
-touch packages/app/node_modules/$FILE_EXCLUDE
-touch packages/app-gql/node_modules/$FILE_EXCLUDE
-touch .oldnestjs9wayproject/$FILE_EXCLUDE
-touch packages_fucked/$FILE_EXCLUDE 
+touch .bak/${FILE_EXCLUDE}
+touch .removed//${FILE_EXCLUDE}
+touch node_modules/${FILE_EXCLUDE}
+touch packages/app-lib/node_modules/${FILE_EXCLUDE}
+touch packages/app/node_modules/${FILE_EXCLUDE}
+touch packages/app-gql/node_modules/${FILE_EXCLUDE}
+touch .oldnestjs9wayproject/${FILE_EXCLUDE}
+touch packages_fucked/${FILE_EXCLUDE} 
+touch packages_removed/${FILE_EXCLUDE}
 
 tar -zcvf $FILE \
-	--exclude-tag-all=$FILE_EXCLUDE \
+	--exclude-tag-all=${FILE_EXCLUDE} \
 	--exclude='FILE|DIR' \
 	.
